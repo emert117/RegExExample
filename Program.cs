05c684ff1fdf4d2646c334599bede584cec97059
e_mert <emert@tx3services.com>
2021-11-30 21:44:24 +0300

    HTML img tag search

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace RegExExample
@@ -7,15 +9,28 @@ namespace RegExExample
     {
         static void Main(string[] args)
         {
-            var s = "long string.....24X10  1111X22   .....1X3";
-            var match = Regex.Match(s, @"\d+X\d+");
-            if (match.Success) {
-                Console.WriteLine(match.Index); // 16
-                Console.WriteLine(match.Value); // 24X10;
-                match = match.NextMatch();
-                Console.WriteLine(match.Value);
+            //var s = "long string.....24X10  1111X22   .....1X3  X 1X X6 99X88 end";
+            //var match = Regex.Match(s, @"\d+X\d+");
+            //while (match.Success)
+            //{
+            //    Console.WriteLine($"Index: {match.Index} Length: {match.Length} Value: {match.Value}");
+            //    match = match.NextMatch();
+            //}
+
+            string fieldValue = "<p>my step 2 green <img src=\"data:image/png;base64,iVBORw0KGgo\" style=\"width: 5px;\" data-filename=\"image.png\"> and"+
+                                " yellow <img class=\"image-class\" src=\"data:image/png;base64,iVBORwmCC\" style=\"width: 5px;\" data-filename=\"image.png\"> inline images</p>";
+            string embeddedImageHeader = "<img src=\"data:image/";
+            string pattern = @"<img\s[^>]*?src\s*=\s*['\""]([^'\""]*?)['\""][^>]*?>";
+            bool isMatch = Regex.IsMatch(fieldValue, pattern);
+            var matches = Regex.Matches(fieldValue, pattern);
+            Dictionary<int,string> markdownsForImages = new Dictionary<int, string>();
+            foreach (var match in matches.ToList())
+            {
+                markdownsForImages.Add(match.Index, $"markdown{match.Index}");
             }
-            Console.WriteLine("Hello World!");
+            var stringVariableMatches = Regex.Replace(fieldValue, pattern, m => markdownsForImages[m.Index]);
+            
+            Console.WriteLine("--end--");
         }
     }
 }
