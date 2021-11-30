0a421bfb9f41e75212065617b52aadb82a553bbc
e_mert <emert@tx3services.com>
2021-11-30 18:03:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegExExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "long string.....24X10  1111X22   .....1X3";
+            var match = Regex.Match(s, @"\d+X\d+");
+            if (match.Success) {
+                Console.WriteLine(match.Index); // 16
+                Console.WriteLine(match.Value); // 24X10;
+                match = match.NextMatch();
+                Console.WriteLine(match.Value);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
